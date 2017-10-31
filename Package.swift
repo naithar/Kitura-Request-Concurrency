29f58db85913dc851c79ae6889adbced2c1c655d
Sergey Minakov <devias.naith@gmail.com>
2017-10-31 20:39:38 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
 	dependencies: [
 		.Package(url: "https://github.com/IBM-Swift-Sunset/Kitura-Request.git", majorVersion: 0),
 		.Package(url: "https://github.com/naithar/Concurrency.git", majorVersion: 0),
-		.Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 16),
+		.Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 17),
 	]
 )
